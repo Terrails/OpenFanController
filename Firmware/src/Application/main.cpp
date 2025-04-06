9c9d8d9f2fdba8cbd39ed82ae611b633ad5a7ab9
Terrails <github@sl.terrails.com>
2025-04-06 18:03:03 +0200

    add short blink every 30 sec to show activity

--- Firmware/src/Application/main.cpp
+++ Firmware/src/Application/main.cpp
@@ -9,12 +9,10 @@
 #define LOG_NAME "MAIN"
 #include "logger.h"
 
-bool led_state = false;
+bool blink_connecting_callback(repeating_timer_t *rt);
+bool blink_active_callback(repeating_timer_t *rt);
 
-bool systick_callback(repeating_timer_t *rt);
-
-int main()
-{
+int main() {
     set_sys_clock_khz(250000, false);
     stdio_init_all();
 
@@ -36,27 +34,42 @@ int main()
 
     LOG_INFO("Up and running");
 
-    repeating_timer_t timer = {0}; // zero out all fields
-    bool blinking = false;
+    repeating_timer_t blink_connecting_timer = {0};
+    bool blink_connecting = false;
+
+    repeating_timer_t blink_active_timer = {0};
+    bool blink_active = false;
 
     while (1) {
         usb_cdc_tick();
 
         bool serial_connected = gpio_get(USB_DET_PIN) && usb_cdc_connected();
         if (!serial_connected) { // Start blinking if not connected
-            if (!blinking) {
+            if (!blink_connecting) {
                 LOG_INFO("USB Disconnected");
-                add_repeating_timer_ms(-1000, systick_callback, NULL, &timer);
-                blinking = true;
+                add_repeating_timer_ms(-1000, blink_connecting_callback, NULL, &blink_connecting_timer);
+                blink_connecting = true;
             }
+
+            if (blink_active) {
+                cancel_repeating_timer(&blink_active_timer);
+                gpio_put(LED_PIN, 0);
+                blink_active = false;
+            }
+
             continue;
         }
 
-        if (blinking) {
+        if (blink_connecting) {
             LOG_INFO("USB Connected");
-            cancel_repeating_timer(&timer);
+            cancel_repeating_timer(&blink_connecting_timer);
             gpio_put(LED_PIN, 0);
-            blinking = false;
+            blink_connecting = false;
+        }
+
+        if (!blink_active) {
+            add_repeating_timer_ms(-30000, blink_active_callback, NULL, &blink_active_timer);
+            blink_active = true;
         }
 
         fan_periodic_tick();
@@ -64,9 +77,17 @@ int main()
     }
 }
 
-bool systick_callback(repeating_timer_t *rt)
-{
-    gpio_put(LED_PIN, led_state);
-    led_state = !led_state;
-    return true; // keep repeating
+bool blink_connecting_callback(repeating_timer_t *rt) {
+    gpio_put(LED_PIN, !static_cast<bool>(gpio_get(LED_PIN)));
+    return true;
+}
+
+bool blink_active_callback(repeating_timer_t *rt) {
+    gpio_put(LED_PIN, 1);
+    // Turn off for a quick blink
+    add_alarm_in_ms(500, [](alarm_id_t id, void *user_data) -> int64_t {
+        gpio_put(LED_PIN, 0);
+        return 0;
+    }, NULL, true);
+    return true;
 }
